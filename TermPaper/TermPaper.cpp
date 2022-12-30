14000c1e5235304bac217af1f865c1755986ceae
Manoela <manoelabarakova03@gmail.com>
2022-12-30 22:07:15 +0200

    First commit
    Added the options menu.

---
+++ TermPaper/TermPaper.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "1. Start game\n" << "2. Leaderboard\n" << "3. Quit\n";
+    std::cout << "Please enter a number to choose one of the options: ";
+    int chosenOption;
+    std::cin >> chosenOption;
+    if (chosenOption == 1) {
+
+    }
+}
