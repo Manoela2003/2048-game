52aacfe52d2d6702ec2a035c557b83933e614785
Manoela <manoelabarakova03@gmail.com>
2023-01-01 16:03:25 +0200

    Added moving left and right logic

--- TermPaper/TermPaper.cpp
+++ TermPaper/TermPaper.cpp
@@ -64,6 +64,13 @@ bool CheckForColumnWithZeros(int** matrix, int dimension, int col) {
 	return true;
 }
 
+bool CheckForRowWithZeros(int** matrix, int dimension, int row) {
+	for (int col = 0; col < dimension; col++)
+		if (matrix[row][col] != 0)
+			return false;
+	return true;
+}
+
 void MovingNumbersUp(int** matrix, int dimension, int col) {
 	for (int counter = 0; counter < dimension - 1; counter++) {
 		for (int row = counter + 1; row > 0; row--) {
@@ -92,6 +99,33 @@ void MovingNumbersDown(int** matrix, int dimension, int col) {
 	}
 }
 
+void MovingNumbersLeft(int** matrix, int dimension, int row) {
+	for (int counter = 0; counter < dimension - 1; counter++) {
+		for (int col = counter + 1; col > 0; col--) {
+			if (matrix[row][col - 1] == matrix[row][col] || matrix[row][col - 1] == 0) {
+				matrix[row][col - 1] += matrix[row][col];
+				matrix[row][col] = 0;
+			}
+			else
+				break;
+		}
+	}
+}
+
+void MovingNumbersRight(int** matrix, int dimension, int row) {
+	for (int counter = dimension - 1; counter > 0; counter--) {
+		for (int col = counter - 1; col < dimension - 1; col++) {
+			if (matrix[row][col + 1] == matrix[row][col] || matrix[row][col + 1] == 0) {
+				matrix[row][col + 1] += matrix[row][col];
+				matrix[row][col] = 0;
+			}
+			else {
+				break;
+			}
+		}
+	}
+}
+
 void MovingNumbers(int** matrix, int dimension, char direction) {
 	if (direction == 'w') {
 		for (int col = 0; col < dimension; col++) {
@@ -105,6 +139,20 @@ void MovingNumbers(int** matrix, int dimension, char direction) {
 			if (CheckForColumnWithZeros(matrix, dimension, col))
 				continue;
 			MovingNumbersDown(matrix, dimension, col);
+		}
+	}
+	else if (direction == 'a') {
+		for (int row = 0; row < dimension; row++) {
+			if (CheckForRowWithZeros(matrix, dimension, row))
+				continue;
+			MovingNumbersLeft(matrix, dimension, row);
+		}
+	}
+	else if (direction == 'd') {
+		for (int row = 0; row < dimension; row++) {
+			if (CheckForRowWithZeros(matrix, dimension, row))
+				continue;
+			MovingNumbersRight(matrix, dimension, row);
 		}
 	}
 }
